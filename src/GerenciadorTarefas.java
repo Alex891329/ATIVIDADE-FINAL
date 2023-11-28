87f337e6805c775f3033fe05ad9bd01b04893a88
Guilhermedbs <117949451+Guilhermedbs@users.noreply.github.com>
2023-11-28 18:11:46 -0300

    Criação da classe Tarefas Completas

--- src/GerenciadorTarefas.java
+++ src/GerenciadorTarefas.java
@@ -26,6 +26,46 @@ public class GerenciadorTarefas {
         System.out.println("- " + tarefa);
     }
 
+    public class TarefasCompletas {
+        private List<String> tarefasConcluidas;
+
+        public TarefasCompletas() {
+            this.tarefasConcluidas = new ArrayList<>();
+        }
+
+        public void concluirTarefa(String tarefa) {
+            if (tarefas.contains(tarefa)) {
+                tarefas.remove(tarefa);
+                tarefasConcluidas.add(tarefa);
+                System.out.println(nomeMembro + " concluiu a seguinte tarefa:");
+                System.out.println("- " + tarefa);
+            } else {
+                System.out.println(nomeMembro + " tentou concluir uma tarefa inexistente: " + tarefa);
+            }
+        }
+
+        public List<String> getTarefasConcluidas() {
+            return tarefasConcluidas;
+        }
+
+        public List<String> tarefasConcluidasPorMembro(List<String> tarefasDoMembro) {
+            List<String> tarefasConcluidas = new ArrayList<>();
+            for (String tarefa : tarefasDoMembro) {
+                // Simulando que uma tarefa é considerada concluída se contiver a palavra "Concluída"
+                if (tarefa.contains("Concluída")) {
+                    tarefasConcluidas.add(tarefa);
+                }
+            }
+            return tarefasConcluidas;
+        }
+
+        public List<String> tarefasPendentes(List<String> todasTarefas, List<String> tarefasConcluidas) {
+            List<String> tarefasPendentes = new ArrayList<>(todasTarefas);
+            tarefasPendentes.removeAll(tarefasConcluidas);
+            return tarefasPendentes;
+        }
+    }
+
     public static void main(String[] args) {
         GerenciadorTarefas guilherme = new GerenciadorTarefas("Guilherme");
         GerenciadorTarefas alex = new GerenciadorTarefas("Alex");
@@ -42,6 +82,22 @@ public class GerenciadorTarefas {
         guilherme.enviarTarefa("Alex", "Revisar código");
         guilherme.enviarTarefa("Elias", "Revisar código");
         alex.listarTarefas();
+
+        GerenciadorTarefas.TarefasCompletas tarefasCompletasGuilherme = guilherme.new TarefasCompletas();
+
+        guilherme.adicionarTarefa("Terminar o seminário");
+        guilherme.listarTarefas();
+
+        tarefasCompletasGuilherme.concluirTarefa("Concluir projeto Java e estudar");
+
+        guilherme.listarTarefas();
+
+        List<String> tarefasConcluidasPorGuilherme = tarefasCompletasGuilherme.getTarefasConcluidas();
+        System.out.println("Tarefas concluídas por Guilherme: " + tarefasConcluidasPorGuilherme);
+
+        List<String> tarefasPendentesGuilherme = tarefasCompletasGuilherme.tarefasPendentes(guilherme.tarefas, tarefasConcluidasPorGuilherme);
+        System.out.println("Tarefas pendentes para Guilherme: " + tarefasPendentesGuilherme);
     }
 }
+
 
